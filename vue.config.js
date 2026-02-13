1f16c32480acd7259f5df251d6cc590a2295ea40
YSuhasReddy <89406245+YSuhasReddy@users.noreply.github.com>
2026-02-13 09:25:32 +0530

    Add GitLab CI config for Pages deployment
    Co-Authored-By: Suhas Reddy, YS <noreply@github.com>

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',
+  publicPath: process.env.CI ? '/virtuoso-d365-crm/' : '/',
   outputDir: 'dist',
   assetsDir: 'assets',
   productionSourceMap: false,
